180a79d797edf4ffdf906b87199de4d15002b098
morank92 <928786096@qq.com>
2018-08-01 18:36:45 +0800

    netty-通信

--- src/main/java/com/mk/controller/SpringbootApplication.java
+++ src/main/java/com/mk/controller/SpringbootApplication.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 
 
+
 @SpringBootApplication // Spring Boot项目的核心注解，主要目的是开启自动配置
 @ComponentScan(basePackages={"com.mk"})
 @MapperScan("com.mk.dao")//将项目中对应的mapper类的路径加进来就可以了
